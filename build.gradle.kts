b6665841d52d069269334733c55cae439444a5f4
apstndb <gamaguchi@gmail.com>
2020-02-06 19:32:58 +0900

    Remove dockerized test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,15 @@
-import java.util.Arrays;
 val kotlinVersion = "1.3.61"
-
-/*
-buildscript {
-    repositories {
-        maven {
-            url = uri("https://plugins.gradle.org/m2/")
-        }
-    }
-    dependencies {
-        classpath("com.pedjak.gradle.plugins:dockerized-test:0.5.10")
-    }
-}
-*/
-
-// apply(plugin = "com.github.pedjak.dockerized-test")
 
 plugins {
     java
     id("com.google.cloud.tools.jib") version "1.8.0"
     kotlin("jvm") version "1.3.61"
-    id("com.github.pedjak.dockerized-test") version "0.5.10"
 }
 
 group = "dev.apstn"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven( url = "http://dl.bintray.com/pedjak/gradle-plugins")
     mavenCentral()
 
 }
@@ -48,13 +30,5 @@ configure<JavaPluginConvention> {
 jib {
     container {
         mainClass = "zetasql.Main"
-    }
-}
-
-tasks {
-    test {
-        val docker: com.pedjak.gradle.plugins.dockerizedtest.DockerizedTestExtension by extensions
-        // val docker = (extensions["docker"] as com.pedjak.gradle.plugins.dockerizedtest.DockerizedTestExtension)
-        docker.image = "openjdk:8"
     }
 }
