2e51ae8116d31195d4d9e35589f7af1dface2c7f
apstndb <gamaguchi@gmail.com>
2020-02-06 20:11:22 +0900

    Implement CREATE TABLE

--- src/main/kotlin/zetasql/Main.kt
+++ src/main/kotlin/zetasql/Main.kt
@@ -2,19 +2,25 @@ package zetasql
 
 import com.google.zetasql.Analyzer
 import com.google.zetasql.AnalyzerOptions
+import com.google.zetasql.LanguageOptions
 import com.google.zetasql.SimpleCatalog
 import com.google.zetasql.resolvedast.ResolvedNodes
 
 object Main {
     fun f(sql: String): String {
         val analyzerOptions = AnalyzerOptions()
+        val languageOptions = LanguageOptions()
+        languageOptions.setSupportsAllStatementKinds()
+        analyzerOptions.languageOptions = languageOptions
         val catalog = SimpleCatalog("global")
         val resolvedStatement = Analyzer(analyzerOptions, catalog).analyzeStatement(sql)
         println(resolvedStatement.nodeKindString())
-        if (resolvedStatement !is ResolvedNodes.ResolvedQueryStmt) {
-            throw Exception("resolvedStatement is ${resolvedStatement.nodeKindString()}")
+        if (resolvedStatement is ResolvedNodes.ResolvedQueryStmt) {
+            return resolvedStatement.outputColumnList.map { s -> "${s.name}:${s.column.type}" }.joinToString(separator=",")
+        } else if (resolvedStatement is ResolvedNodes.ResolvedCreateTableStmtBase) {
+            return resolvedStatement.columnDefinitionList.map { column -> "${column.name}:${column.type}" }.joinToString(separator = ",")
         }
-        return resolvedStatement.outputColumnList.map { s -> "${s.name}:${s.column.type}" }.joinToString(separator=",")
+        throw Exception("resolvedStatement is ${resolvedStatement.nodeKindString()}")
     }
 
     @JvmStatic
@@ -24,12 +30,5 @@ object Main {
         } catch(e: Exception) {
             println(e)
         }
-        /*
-        resolvedStatement.accept(object: Visitor() {
-            override fun visit(node: ResolvedNodes.ResolvedQueryStmt) {
-                node.outputColumnList
-            }
-        })
-         */
     }
 }
