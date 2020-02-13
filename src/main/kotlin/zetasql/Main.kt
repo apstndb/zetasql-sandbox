4246c505869154caa50062db4bb6977113e910aa
apstndb <gamaguchi@gmail.com>
2020-02-14 00:11:37 +0900

    Add formatter

--- src/main/kotlin/zetasql/Main.kt
+++ src/main/kotlin/zetasql/Main.kt
@@ -1,24 +1,38 @@
 package zetasql
 
-import com.google.zetasql.Analyzer
-import com.google.zetasql.AnalyzerOptions
-import com.google.zetasql.LanguageOptions
-import com.google.zetasql.SimpleCatalog
+import com.google.zetasql.*
 import com.google.zetasql.resolvedast.ResolvedNodes
 
 object Main {
+    fun format(sql: String): String {
+        return SqlFormatter().formatSql(sql);
+    }
+
     fun f(sql: String): String {
         val analyzerOptions = AnalyzerOptions()
         val languageOptions = LanguageOptions()
         languageOptions.setSupportsAllStatementKinds()
         analyzerOptions.languageOptions = languageOptions
         val catalog = SimpleCatalog("global")
-        val resolvedStatement = Analyzer(analyzerOptions, catalog).analyzeStatement(sql)
+        val analyzer = Analyzer(analyzerOptions, catalog)
+        val parseResumeLocation = ParseResumeLocation(sql)
+        val resolvedStatements = ArrayList<ResolvedNodes.ResolvedStatement>()
+        // TODO: justify loop condition
+        while (parseResumeLocation.bytePosition != sql.length) {
+            resolvedStatements.add(analyzer.analyzeNextStatement(parseResumeLocation))
+        }
+        return resolvedStatements.joinToString("\n") { resolvedStmt -> toString(resolvedStmt)}
+    }
+
+    private fun toString(resolvedStatement: ResolvedNodes.ResolvedStatement): String {
         println(resolvedStatement.nodeKindString())
-        if (resolvedStatement is ResolvedNodes.ResolvedQueryStmt) {
-            return resolvedStatement.outputColumnList.map { s -> "${s.name}:${s.column.type}" }.joinToString(separator=",")
-        } else if (resolvedStatement is ResolvedNodes.ResolvedCreateTableStmtBase) {
-            return resolvedStatement.columnDefinitionList.map { column -> "${column.name}:${column.type}" }.joinToString(separator = ",")
+        when (resolvedStatement) {
+            is ResolvedNodes.ResolvedQueryStmt ->
+                return resolvedStatement.outputColumnList.joinToString(",") { column -> "${column.name}:${column.column.type}" }
+            is ResolvedNodes.ResolvedCreateTableStmtBase ->
+                return resolvedStatement.columnDefinitionList.joinToString(",") { columnDef -> "${columnDef.name}:${columnDef.type}" }
+            is ResolvedNodes.ResolvedCreateIndexStmt -> {
+            }
         }
         throw Exception("resolvedStatement is ${resolvedStatement.nodeKindString()}")
     }
@@ -26,7 +40,14 @@ object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         try {
-            println(f(args[0]))
+            val input = generateSequence(::readLine).joinToString("\n")
+            println(input)
+            val str = when (args[0]) {
+                "format" -> format(input)
+                "parse" -> f(input)
+                else -> throw Exception("unknown:" + args[0])
+            }
+            println(str)
         } catch(e: Exception) {
             println(e)
         }
