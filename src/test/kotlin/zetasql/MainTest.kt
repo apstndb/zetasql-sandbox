ac2d121b18af1a1358787bdd01996b9c1e02f20e
apstndb <gamaguchi@gmail.com>
2020-03-07 17:24:45 +0900

    Enable GitHub Actions

--- src/test/kotlin/zetasql/MainTest.kt
+++ src/test/kotlin/zetasql/MainTest.kt
@@ -8,6 +8,11 @@ class MainTest {
     fun testF() {
         val s = Main.analyze("SELECT * FROM (SELECT 1 AS x)")
         assertEquals("x:INT64", s)
-        assertEquals("x:STRING", s)
+    }
+
+    @Test
+    fun testExtractTable() {
+        val s = Main.extractTable("SELECT * FROM `project.dataset.table`")
+        assertEquals("project.dataset.table", s)
     }
 }
