6ba5eec21d48abc90bea983dd091b8fe29fecbd4
apstndb <gamaguchi@gmail.com>
2020-05-01 21:19:15 +0900

    Update ZetaSQL to 2020.04.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import kotlin.collections.mapOf
 val kotlinVersion = "1.3.61"
+val zetasqlVersion = "2020.04.1"
 
 plugins {
     java
     id("com.google.cloud.tools.jib") version "1.8.0"
     kotlin("jvm") version "1.3.61"
+    application
 }
 
 group = "dev.apstn"
@@ -12,13 +14,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-
 }
+
 dependencies {
     implementation(group="com.google.cloud", name="google-cloud-bigquery", version="1.108.1")
-    implementation(group= "com.google.zetasql", name= "zetasql-client", version= "2020.03.2")
-    implementation(group= "com.google.zetasql", name= "zetasql-types", version= "2020.03.2")
-    implementation(group= "com.google.zetasql", name= "zetasql-jni-channel", version= "2020.03.2")
+    implementation(group= "com.google.zetasql", name= "zetasql-client", version=zetasqlVersion)
+    implementation(group= "com.google.zetasql", name= "zetasql-types", version=zetasqlVersion)
+    implementation(group= "com.google.zetasql", name= "zetasql-jni-channel", version=zetasqlVersion)
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("junit:junit:4.12")
     testImplementation(group="org.jetbrains.kotlin", name="kotlin-test-junit",version="1.3.61")
@@ -34,3 +36,8 @@ jib {
         environment = mapOf(Pair("SUPPRESS_GCLOUD_CREDS_WARNING", "true"))
     }
 }
+
+application {
+    mainClassName = "zetasql.Main"
+}
+
